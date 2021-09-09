e9bb1b9f33d134c037e9c43a74a7cf33d324776a
John Kevin <johnkevindejesus01@gmail.com>
2021-09-09 20:59:20 +0800

    fixed the error in removeManyPeople again

--- myApp.js
+++ myApp.js
@@ -87,9 +87,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  Person.remove({ name: nameToRemove}, (err, people)=> {
-    if(err) return done(err);
-    return done(null, people);
+  Person.remove({name: nameToRemove}, (err, response) => {
+    if(err) return console.log(err);
+    done(null, response);
   })
 };
 
